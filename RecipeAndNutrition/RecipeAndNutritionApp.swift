2c6f6ac8ba9b66267ad9d0b92d45020466eb7029
AIWASS23 <marcelodearaujo677@gmail.com>
2024-07-01 09:53:43 -0300

    Initial Commit

---
+++ RecipeAndNutrition/RecipeAndNutritionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAndNutritionApp.swift
+//  RecipeAndNutrition
+//
+//  Created by marcelodearaujo on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAndNutritionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
